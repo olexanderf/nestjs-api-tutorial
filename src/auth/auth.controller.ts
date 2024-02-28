bfc3d99ed003f941d2efacd2dc5d48ac7100d64a
olexanderf <alex1990sfreem@gmail.com>
2024-02-28 22:51:47 +0200

    jwt, guard, users/me

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto';
 
@@ -9,6 +9,7 @@ export class AuthController {
   signUp(@Body() dto: AuthDto) {
     return this.authService.signUp(dto);
   }
+  @HttpCode(HttpStatus.OK)
   @Post('signin')
   signIn(@Body() dto: AuthDto) {
     return this.authService.signIn(dto);
